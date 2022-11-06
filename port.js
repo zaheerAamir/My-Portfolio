8c122de884c54f86616c190cbabd6be7c9f81c16
Aamir Zaheer <aamirzaheer95@gmail.com>
2022-11-06 20:14:02 +0530

    new project added

--- port.js
+++ port.js
@@ -5,6 +5,7 @@ const butt = document.querySelector('#LtoD')
 const ball = document.querySelector('.ball')
 const bod = document.querySelector('body')
 
+
 /* navlist:  */
 const dod = document.querySelector('.aOne')
 const nop = document.querySelector('.aTwo')
@@ -12,6 +13,10 @@ const fod = document.querySelector('.aThree')
 
 /* title sudoku solver in the projects section (p tag) */
 const proj = document.querySelector('.title')
+const proj2 = document.querySelector('.title2')
+const proj5 = document.querySelector('.title5')
+
+const dup = document.querySelector('.proj')
 
 /* button About me*/
 const tubb = document.querySelector('#abtme')
@@ -29,7 +34,10 @@ const arr = [
         nop.classList.toggle('dark')
         fod.classList.toggle('dark')
         proj.classList.toggle('dark')
+        proj2.classList.toggle('dark')
+        proj5.classList.toggle('dark')
         tubb.classList.toggle('dark')
+        dup.classList.toggle('dark')
         console.log(butt, 'clicked')
     }
 ]
